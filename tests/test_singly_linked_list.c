1ee826b4c6cab5e6ada4a9d6a688be78db5288b3
TejasR <sajetr@gmail.com>
2015-01-01 11:19:53 +0530

    Added comments

--- tests/test_singly_linked_list.c
+++ tests/test_singly_linked_list.c
@@ -10,10 +10,17 @@ static char* test_create_normal_op() {
     return 0;
 }
 
+/**
+ * Tests:
+ * 1. When list is empty
+ * 2. When list is not empty
+ */
 static char* test_insert_head_normal_op() {
     node *first = NULL;
+    /* List empty */
     mu_assert("error, unable to insert item into the list", insert_head(&first, 10) == 0);
     mu_assert("error, item not inserted at the head", first->item == 10);
+    /* List not empty */
     mu_assert("error, unable to insert item into the list", insert_head(&first, 12) == 0);
     mu_assert("error, item not inserted at the head", first->item == 12);
     delete_head(&first);
@@ -21,10 +28,17 @@ static char* test_insert_head_normal_op() {
     return 0;
 }
 
+/**
+ * Tests:
+ * 1. When list is empty
+ * 2. When list is not empty
+ */
 static char* test_insert_tail_normal_op() {
     node *first = NULL;
+    /* List empty */
     mu_assert("error, unable to insert item into the list", insert_tail(&first, 10) == 0);
     mu_assert("error, item cannot be found", first->item == 10);
+    /* List not empty */
     mu_assert("error, unable to insert item into the list", insert_tail(&first, 12) == 0);
     node *temp = first;
     while (temp->next != NULL) {
@@ -47,25 +61,33 @@ static char* test_free_node_normal_op() {
 
 static char* test_delete_head_normal_op() {
     node *first = NULL;
+    /* List empty */
     mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == -3);
+    /* List not empty */
     insert_head(&first, 10);
     insert_head(&first, 12);
     mu_assert("error, unable to free the memory being used by the node", delete_head(&first) == 12);
     insert_tail(&first, 14);
     mu_assert("error, unable to free the memory being used by the node", delete_head(&first) == 10);
-    delete_head(&first);
+    mu_assert("error, unable to free the memory being used by the node", delete_head(&first) == 14);
+    /* List empty */
+    mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == -3);
     return 0;
 }
 
 static char* test_delete_tail_normal_op() {
     node *first = NULL;
+    /* List empty */
     mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == -3);
+    /* List not empty */
     insert_head(&first, 10);
     insert_head(&first, 12);
     mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == 10);
     insert_tail(&first, 14);
     mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == 14);
-    delete_head(&first);
+    mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == 12);
+    /* List empty */
+    mu_assert("error, unable to free the memory being used by the node", delete_tail(&first) == -3);
     return 0;
 }
 
